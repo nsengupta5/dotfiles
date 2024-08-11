26a3d69fa47de4ea6cff5323fad6b3f2b9a0a70c
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2024-08-11 13:56:52 +0800

    Impt change to lua

--- init.lua
+++ init.lua
@@ -172,7 +172,14 @@ lsp_zero.extend_lspconfig({
 
 -- Mason settings
 require("mason").setup({ ui = { icons = { package_installed = "✓", package_pending = "➜", package_uninstalled = "✗" } } })
-require("mason-lspconfig").setup({ ensure_installed = {'lua_ls', 'rust_analyzer'} })
+require("mason-lspconfig").setup({
+    ensure_installed = {'lua_ls', 'rust_analyzer'},
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    }
+})
 
 -- Completion settings
 require('cmp').setup({
